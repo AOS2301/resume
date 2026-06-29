f3650bd72eec17b9b07ba5fc2c4478faf7243239
Arthur <schmertzs2@gmail.com>
2026-06-29 12:56:32 -0300

    Correção da responsividade do projeto

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,6 +1,22 @@
 <script setup>
-import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+import { RouterLink, useRouter } from 'vue-router'
 import '../assets/nav-bar.css'
+
+const isOpen = ref(false)
+const router = useRouter()
+
+function toggleMenu() {
+  isOpen.value = !isOpen.value
+  document.body.style.overflow = isOpen.value ? 'hidden' : ''
+}
+
+function closeMenu() {
+  isOpen.value = false
+  document.body.style.overflow = ''
+}
+
+router.afterEach(() => closeMenu())
 </script>
 
 <template>
@@ -8,9 +24,22 @@ import '../assets/nav-bar.css'
     <div class="navbar-logo">
       <RouterLink to="/" class="logo-link">AOS.DEV</RouterLink>
     </div>
-    <nav class="navbar-nav">
-      <RouterLink to="/" class="nav-link">SUMMARY</RouterLink>
-      <RouterLink to="/projects" class="nav-link">PROJECTS</RouterLink>
+
+    <nav class="navbar-nav" :class="{ 'is-open': isOpen }">
+      <RouterLink to="/" class="nav-link" @click="closeMenu">SUMMARY</RouterLink>
+      <RouterLink to="/projects" class="nav-link" @click="closeMenu">PROJECTS</RouterLink>
     </nav>
+
+    <button
+      class="nav-toggle"
+      :class="{ 'is-open': isOpen }"
+      :aria-label="isOpen ? 'Fechar menu' : 'Abrir menu'"
+      :aria-expanded="isOpen"
+      @click="toggleMenu"
+    >
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
   </header>
 </template>
